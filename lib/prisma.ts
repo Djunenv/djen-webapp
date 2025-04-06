325df2c890ccf51cfd885bcc661f7e14b7ce02e9
Djunenv <dejunk911@gmail.com>
2025-04-06 11:12:03 +0000

    Ensure correct imports and file paths

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-let prisma: any;
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+const prisma: PrismaClient =
+  process.env.NODE_ENV === "production"
+    ? new PrismaClient()
+    : global.prisma || (global.prisma = new PrismaClient());
+
 export default prisma;
